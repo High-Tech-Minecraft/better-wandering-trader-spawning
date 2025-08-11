b1d44d516d016e8bfa3107ff9bc8931a84a37c1f
Joseph Calabro <beebfish49@gmail.com>
2025-08-11 13:51:46 -0600

    adds helpful comments to the default config file

--- src/main/java/net/paishofish49/betterwanderingtraderspawning/MyConfigProvider.java
+++ src/main/java/net/paishofish49/betterwanderingtraderspawning/MyConfigProvider.java
@@ -3,6 +3,11 @@ package net.paishofish49.betterwanderingtraderspawning;
 public class MyConfigProvider implements SimpleConfig.DefaultConfig {
     @Override
     public String get(String namespace) {
-        return "wandering.trader.spawn.delay=24000\nwandering.trader.spawn.timer=1200";
+        return """
+                # defaults to 24000 (20 irl minutes)
+                wandering.trader.spawn.delay=24000
+                
+                # defaults to 1200 (1 irl minute)
+                wandering.trader.spawn.timer=1200""";
     }
 }
